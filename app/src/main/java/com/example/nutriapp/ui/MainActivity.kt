e3ea136eb3c3aa6d7de71ecbd5340eff7b6726ea
Flavio Henriquez <fla.henriquez@duocuc.cl>
2025-10-21 00:19:21 -0300

    add login

---
+++ app/src/main/java/com/example/nutriapp/ui/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.example.nutriapp.ui
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import com.example.nutriapp.ui.screen.LoginScreen
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            LoginScreen()
+
+
+
+                }
+            }
+        }
