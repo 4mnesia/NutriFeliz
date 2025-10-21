5e8e22e29a97de937e227fccfb026575bcdad8cc
Flavio Henriquez <fla.henriquez@duocuc.cl>
2025-10-21 00:19:21 -0300

    cambio de colores

---
+++ app/src/main/java/com/example/nutriapp/ui/screen/LoginScreen.kt
@@ -0,0 +1,73 @@
+package com.example.nutriapp.ui.screen
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.res.painterResource
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.IntrinsicSize
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Button
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.VerticalDivider
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.AlignmentLine
+
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+
+import androidx.compose.ui.unit.sp
+import com.example.nutriapp.R.drawable.nutrialogo
+
+@Composable
+fun LoginScreen() {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+
+    ) {
+        Image(
+            painter = painterResource(id = nutrialogo),
+            contentDescription = "Login Image",
+            modifier = Modifier.size(200.dp)
+        )
+
+        Text(text = "Bienvenido a NutriAPP", fontSize = 28.sp, fontWeight = FontWeight.Bold)
+
+        Spacer(modifier = Modifier.height(4.dp))
+
+        Text(text = "Inicia tu sesion")
+
+        OutlinedTextField(value = "", onValueChange = {}, label = {
+            Text(text = "Tu email")
+        })
+        Spacer(modifier = Modifier.height(4.dp))
+        OutlinedTextField(value = "", onValueChange = {}, label = {
+            Text(text = "Tu contraseña")
+        })
+
+        Spacer(modifier = Modifier.height(16.dp))
+        Button(onClick = { }) {
+            Text(text = "Iniciar Sesion")
+        }
+
+        Spacer(modifier = Modifier.height(16.dp))
+        Button(onClick = { }) {
+            Text(text = "Registrate")
+        }
+
+    }
+}
+
+
+
+
+
--- app/src/main/java/com/example/nutriapp/ui/theme/Color.kt
+++ app/src/main/java/com/example/nutriapp/ui/theme/Color.kt
@@ -3,9 +3,13 @@ package com.example.nutriapp.ui.theme
 import androidx.compose.ui.graphics.Color
 
 val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
+val PurpleGrey80 = Color(0xFFAA6FFF)
 val Pink80 = Color(0xFFEFB8C8)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val Buttons = Color(0xFF997EE6)
+
+val Border = Color(0xFF3f2e65)
+val Content1 = Color(0xFF1a0f2e)
+
+val magent = Color(0xFFa748e3)
+val PurpleClare = Color(0xFF241146)
