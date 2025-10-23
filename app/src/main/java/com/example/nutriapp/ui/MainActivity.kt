e80b4d34b4f45a6729faa1e1f9ef30b482639496
4mnesia <134973064+4mnesia@users.noreply.github.com>
2025-10-22 21:42:54 -0300

    login beta
    intentando mejorar el login

--- app/src/main/java/com/example/nutriapp/ui/MainActivity.kt
+++ app/src/main/java/com/example/nutriapp/ui/MainActivity.kt
@@ -4,15 +4,15 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import com.example.nutriapp.ui.screen.LoginScreen
+import com.example.nutriapp.ui.theme.NutriAppTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            LoginScreen()
-
-
-
-                }
+            NutriAppTheme {
+                LoginScreen()
             }
         }
+    }
+}
