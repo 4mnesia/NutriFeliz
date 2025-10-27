a0be01bcd9ceb5a879a9973541ce60264c2e186c
pedcamp <ped.campos@duocuc.cl>
2025-10-27 00:26:03 -0300

    .

--- app/src/main/java/com/example/nutriapp/ui/theme/Theme.kt
+++ app/src/main/java/com/example/nutriapp/ui/theme/Theme.kt
@@ -49,7 +49,6 @@ private val LightColorScheme = lightColorScheme(
 @Composable
 fun NutriAppTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
-    // Dynamic color is available on Android 12+
     dynamicColor: Boolean = true,
     content: @Composable () -> Unit
 ) {
