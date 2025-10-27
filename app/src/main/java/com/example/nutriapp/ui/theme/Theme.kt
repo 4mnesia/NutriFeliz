856694c36c99686a2e969fb43041831e6c0864fc
pedcamp <ped.campos@duocuc.cl>
2025-10-27 00:25:46 -0300

    nose

--- app/src/main/java/com/example/nutriapp/ui/theme/Theme.kt
+++ app/src/main/java/com/example/nutriapp/ui/theme/Theme.kt
@@ -1,6 +1,5 @@
 package com.example.nutriapp.ui.theme
 
-import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
