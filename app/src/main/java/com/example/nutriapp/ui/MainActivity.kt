343b7d44b557ed65cdada2a0708ac4e9fc5365e6
pedcamp <ped.campos@duocuc.cl>
2025-10-27 00:36:47 -0300

    cbjhqkasc

--- app/src/main/java/com/example/nutriapp/ui/MainActivity.kt
+++ app/src/main/java/com/example/nutriapp/ui/MainActivity.kt
@@ -3,7 +3,8 @@ package com.example.nutriapp.ui
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import com.example.nutriapp.ui.screen.LoginScreen
+import com.example.nutriapp.ui.screen.home.HomeScreen
+import com.example.nutriapp.ui.screen.home.LoginScreen
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
