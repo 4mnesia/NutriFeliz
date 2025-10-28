7784c06d6e53349df18b5e1acec7189fc20cdf60
FabianMolinaa <fa.molinao@duocuc.cl>
2025-10-27 22:23:22 -0300

    Foto de perfil
    Elegir imagen de galeria para foto de perfil

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -41,6 +41,7 @@ android {
 }
 
 dependencies {
+    implementation("io.coil-kt:coil-compose:2.6.0")
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.9.4")
     implementation("androidx.compose.material:material-icons-extended:1.7.8")
     implementation(libs.androidx.core.ktx)
